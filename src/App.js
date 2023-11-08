eb58b23c9686b48a7a234c33de2548b6a616512f
Nyxon1 <konrad.nyk@gmail.com>
2023-11-08 14:15:16 +0100

    Improved image selection function for generating variations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import Modal from "./components/modal"
+import { useState } from "react";
+import Modal from "./components/modal";
 import logo from './logo.png';
 
 
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,31 +1,33 @@
-import {useState, useRef} from "react"
+import React, { useState, useRef } from "react";
 
-const Modal = ({setModalOpen, setSelectedImage, selectedImage, generateVariations}) => {
-    const [ error, setError ] = useState(null)
-    const ref = useRef(null)
+const Modal = ({ setModalOpen, setSelectedImage, selectedImage, generateVariations }) => {
+  const [error, setError] = useState(null);
+  const ref = useRef(null);
 
-    const closeModal = () => {
-        setModalOpen(false)
-        setSelectedImage(null)
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedImage(null);
+  };
+
+  const checkSize = () => {
+    if (ref.current && ref.current.naturalWidth == 256 && ref.current.naturalHeight == 256) {
+      generateVariations();
+    } else {
+      setError('Error: Choose a 256 x 256 image');
     }
+  };
 
-    const checkSize = () => {
-        if (ref.current.width <= 256 && ref.current.height <= 256) {
-        } else {
-             setError('Error: Choose 256 x 256 image')
-        }
-    }
-    return (
-        <div className="modal">
-            <div onClick={closeModal}>✖</div>
-            <div className="img-container">
-                {selectedImage && <img ref={ref} src={URL.createObjectURL(selectedImage)} alt="uploaded image"/>}
-            </div>
-            <p>{error || "* Image must be 256 x 256 px"}</p>
-            {!error && <button onClick={checkSize}>Generate</button>}
-            {error && <button onClick={closeModal}>Close this and try again</button>}
-        </div>
-    ) 
-}
+  return (
+    <div className="modal">
+      <div onClick={closeModal}>✖</div>
+      <div className="img-container">
+        {selectedImage && <img ref={ref} src={URL.createObjectURL(selectedImage)} alt="uploaded image" onLoad={checkSize} />}
+      </div>
+      <p>{error || "* Image must be 256 x 256 px"}</p>
+      {!error && <button onClick={checkSize}>Generate</button>}
+      {error && <button onClick={closeModal}>Close this and try again</button>}
+    </div>
+  );
+};
 
-export default Modal
+export default Modal;
